4f5e3fcc8f84e62420aef68b07af3c2d0314cbdf
Alan Binu <alanbinu5@gmail.com>
2021-11-04 11:27:51 +0530

    Create LoaderLogo.js

---
+++ LoaderLogo.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./heart.css";
+
+class LogoLoader extends React.Component {
+  render() {
+    document.addEventListener("mousemove", function (e) {
+      var body = document.querySelector("body");
+      var heart = document.createElement("span1");
+      var x = e.offsetX;
+      var y = e.offsetY;
+      heart.style.left = x + "px";
+      heart.style.top = y + "px";
+      var size = Math.random() * 100;
+      heart.style.width = 20 + size + "px";
+      heart.style.height = 20 + size + "px";
+      // if(window.location.href=="https://alanbinu-portfolio.web.app/#/" || window.location.href=="https://alanbinu-portfolio.web.app/#/splash"){
+      //   body.appendChild(heart);
+      // }
+      if(window.location.href=="http://localhost:3000/#/" || window.location.href=="http://localhost:3000/#/splash"){
+        body.appendChild(heart);
+      }
+      setTimeout(function () {
+        heart.remove();
+      }, 2000);
+    });
+
+    const theme = this.props.theme;
+    return (
+    <>
+    <div id="text"><span>No Matter How Hard it is ,</span> Just Keep Going</div>
+    <div class="loader"></div>
+    </>
+    );
+  }
+}
+
+export default LogoLoader;
